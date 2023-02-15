378869fd56be525707563e81de97d62a296840cc
BrdM <63228054+BrdML@users.noreply.github.com>
2023-02-15 23:28:28 +0100

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
 import logo from './logo.svg';
 import './App.css';
+import { 
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+ } from 'react-router-dom';
+ import Register from './pages/Register';
+ import Login from './pages/Login';
+ import Home from './pages/Home';
+ import Single from './pages/Single';
+ import Admin from './pages/Admin';
+ import Navbar from './components/Navbar';
+ import Footer from './components/Footer';
+
+ const Layout = () => {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+//  Création des routes
+ const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children:[
+      {
+        path:"/",
+        element:<Home />
+      },
+      {
+        path:"/single",
+        element:<Single />
+      },
+      {
+        path:"/admin",
+        element:<Admin />
+      },
+    ]
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/home",
+    element: <Home/>,
+  },
+  {
+    path: "/single",
+    element: <Single/>,
+  },
+  {
+    path: "/admin",
+    element: <Admin/>,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* route racin, toute les routes passe a l'interieur */}
+      <RouterProvider router={router} />
     </div>
   );
 }
