558fd5a9b6549397150e9c703ed9da8eb9dd682c
BrdM <63228054+BrdML@users.noreply.github.com>
2023-02-18 22:18:48 +0100

    single page
    creation:
    page des articles
    modification:
    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
+
+  const posts = [
+    {
+      id: 1,
+      title: "Lorem ipsum dolor sit amet",
+      desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur doloribus possimus nam sequi? ",
+      img: "https://images.pexels.com/photos/39284/macbook-apple-imac-computer-39284.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+    },
+    {
+      id: 2,
+      title: "Lorem ipsum dolor sit amet",
+      desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur doloribus possimus nam sequi? ",
+      img: "https://images.pexels.com/photos/2582935/pexels-photo-2582935.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+    },
+    {
+      id: 3,
+      title: "Lorem ipsum dolor sit amet",
+      desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur doloribus possimus nam sequi? ",
+      img: "https://images.pexels.com/photos/3944417/pexels-photo-3944417.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+    },
+    {
+      id: 4,
+      title: "Lorem ipsum dolor sit amet",
+      desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur doloribus possimus nam sequi? ",
+      img: "https://images.pexels.com/photos/909256/pexels-photo-909256.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+    }
+  ]
   return (
-    <div>Home</div>
-  )
-}
+    <div className='home'>
+      <div className="posts">
+        {posts.map(post=>(
+          <div className="post" key={post.id}>
+            <div className="img">
+              <img src={post.img} alt="" />
+              <div className="bg"></div>
+            </div>
+            <div className="content">
+              <Link className="link" to={`/post/${post.id}`}>
+                <h1>{post.title}</h1>
+              </Link>
+              <p>{post.desc}</p>
+              <button to={`/post/${post.id}`}>Lire pluss..</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Home
--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Edit from '../images/editer.png';
+import Delete from '../images/delete.png'
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className='single'>
+      <div className="content">
+        <img src="https://images.pexels.com/photos/1612351/pexels-photo-1612351.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+        <div className="user">
+          <img src="https://images.pexels.com/photos/1851164/pexels-photo-1851164.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" />
+          <div className="info">
+            <span>Kyle</span>
+            <p>Posté il y a jours</p>
+          </div>
+          <div className="edit">
+            <Link to={`/admin?edit=`}>
+              <img src={Edit} alt="edit" />
+            </Link>
+            <Link>
+              <img src={Delete} alt="delete" />
+            </Link>
+          </div>
+        </div>
+        <h1>Lorem, ipsum dolor sit amet consectetur</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur 
+          adipisicing elit. Ab cumque dolor minus est 
+          iste hic cum, mollitia voluptatum adipisci eaque 
+          dignissimos nulla sunt perspiciatis, eligendi, 
+          consequuntur autem ipsum ad nostrum! Lorem ipsum 
+          dolor sit amet, consectetur adipisicing elit. Ab 
+          corporis quidem consequuntur perferendis atque corrupti, 
+          id vero nemo doloribus! Inventore dolore iusto error doloremque 
+          facilis, quaerat totam laborum rerum corrupti?
+          <br />
+          <br />
+          Lorem, ipsum dolor sit amet consectetur 
+          adipisicing elit. Ab cumque dolor minus est 
+          iste hic cum, mollitia voluptatum adipisci eaque 
+          dignissimos nulla sunt perspiciatis, eligendi, 
+          consequuntur autem ipsum ad nostrum! Lorem ipsum 
+          dolor sit amet, consectetur adipisicing elit. Ab 
+          corporis quidem consequuntur perferendis atque corrupti, 
+          id vero nemo doloribus! Inventore dolore iusto error doloremque 
+          facilis, quaerat totam laborum rerum corrupti?
+          Lorem, ipsum dolor sit amet consectetur 
+          adipisicing elit. Ab cumque dolor minus est 
+          iste hic cum, mollitia voluptatum adipisci eaque 
+          dignissimos nulla sunt perspiciatis, eligendi, 
+          consequuntur autem ipsum ad nostrum! Lorem ipsum 
+          dolor sit amet, consectetur adipisicing elit. Ab 
+          corporis quidem consequuntur perferendis atque corrupti, 
+          id vero nemo doloribus! Inventore dolore iusto error doloremque 
+          facilis, quaerat totam laborum rerum corrupti?
+          </p>
+      </div>
+      <div className="menu">m</div>
+    </div>
   )
 }
 
